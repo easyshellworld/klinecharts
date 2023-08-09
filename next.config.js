84fbd6569c83c493d35a8bcbbde78845dcc7b233
Your Name <you@example.com>
2023-08-10 01:21:28 +0800

    add webkline data && add nelify.toml

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     async rewrites() { 
       return [ 
        //接口请求 前缀带上/api-text/
-        { source: '/api-text', destination: `https://api.mytokenapi.com/currency/kline?com_id=btc_usdt&symbol=btc&anchor=USDT&time="+last+"&market_id=338&period=1d&timestamp=1674739035146&code=ebc161c4c01e448626c3cc30518009d6&platform=web_pc&v=1.0.0&language=en_US&legal_currency=USD` }, 
+        { source: '/currency/:path*', destination: `https://api.mytokenapi.com/currency/:path*` }, 
   
       ]
     },
