346b5fd89e771c81996af6a6f46f1948f232d88e
Your Name <you@example.com>
2023-09-09 05:07:26 +0800

    change layout

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  const router = useRouter()
+ // const router = useRouter()
 
   return (
     <html lang="en">
@@ -35,8 +35,8 @@ export default function RootLayout({ children }) {
       <a className="btn btn-ghost normal-case text-xl">KlineCharts tool</a>
     </div>
     <div className="navbar-end">
-      <button className="btn btn-ghost btn-circle" onClick={()=>{
-        router.push('https://unbug.github.io/codelf')}}>
+      <button className="btn btn-ghost btn-circle" /* onClick={()=>{
+        router.push('https://unbug.github.io/codelf')}} */>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>  
       </button>
       <button className="btn btn-ghost btn-circle">
