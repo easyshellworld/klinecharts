4ee63fee467e9fd381c6df47480be7e90a08846b
shell <shell@example.com>
2023-09-17 02:15:20 +0800

    change one && change back-black

--- app/charts/page.js
+++ app/charts/page.js
@@ -1,6 +1,6 @@
 "use client"
 import { createChart, ColorType } from 'lightweight-charts';
-import React, { useEffect, useRef,useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'next/navigation'
 
 
@@ -10,9 +10,9 @@ export const ChartComponent = props => {
 		data,
 		data2,
 		colors: {
-			backgroundColor = 'white',
+			backgroundColor = 'black',
 			lineColor = '#2962FF',
-			textColor = 'black',
+			textColor = 'white',
 			areaTopColor = '#2962FF',
 			areaBottomColor = 'rgba(41, 98, 255, 0.28)',
 		} = {},
@@ -52,7 +52,7 @@ export const ChartComponent = props => {
 			areaSeries.setData(data2);
 
 			const newSeries = chart.addCandlestickSeries({ lineColor, topColor: areaTopColor, bottomColor: areaBottomColor });
-			
+
 			newSeries.setData(data);
 
 			window.addEventListener('resize', handleResize);
@@ -64,7 +64,7 @@ export const ChartComponent = props => {
 				chart2.remove();
 			};
 		},
-	    [data,data2, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]
+		[data, data2, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]
 	);
 
 	return (
@@ -75,13 +75,13 @@ export const ChartComponent = props => {
 };
 
 
-export function getklinevelue(kline){
-	return  kline.map(a => {
-		if(a.open>a.close){
-		 return	{time:a.time,value:a.volumefrom, color: 'red'}
+export function getklinevelue(kline) {
+	return kline.map(a => {
+		if (a.open > a.close) {
+			return { time: a.time, value: a.volumefrom, color: 'red' }
 		}
-		else{
-			return	{time:a.time,value:a.volumefrom}
+		else {
+			return { time: a.time, value: a.volumefrom }
 		}
 	});
 }
@@ -89,42 +89,45 @@ export function getklinevelue(kline){
 
 export default function Charts(props) {
 	//const search = useSearchParams().get('coin')
-    const geturldata={
-        coin: useSearchParams().get('coin'),
-        day:useSearchParams().get('day')
-    }
-	const [newdata, setnewdata] = useState( [
-		]) 
-  const [newdata2, setnewdata2] = useState( [
-		]) 
-		let today=new Date().getTime()
-		
-		let last=Math.floor(today/1000)
-	fetch('/currency/kline?com_id='+geturldata.coin+'_usdt&symbol='+geturldata.coin+'&anchor=USDT&time='+last+'&market_id=338&period='+geturldata.day+'&timestamp=1674739035146&code=ebc161c4c01e448626c3cc30518009d6&platform=web_pc&v=1.0.0&language=en_US&legal_currency=USD')
-	.then( res=>res.json() ) 
-	.then( data=>{
-          // console.log(data.data.kline)
-		 const data_json=data.data.kline.reverse()
-		  let klinedata={
-			kline:data_json,
-			value:getklinevelue(data_json)
-		}
+	const geturldata = {
+		coin: useSearchParams().get('coin'),
+		day: useSearchParams().get('day')
+	}
+	const [newdata, setnewdata] = useState([
+	])
+	const [newdata2, setnewdata2] = useState([
+	])
+	const [stoploding, setstoploding] = useState(false)
+	if (stoploding == false) {
+		let today = new Date().getTime()
 
-		//console.log(JSON.stringify(klinedata))
-	    setnewdata(klinedata.kline)
-		setnewdata2(klinedata.value)
-		// console.log(newdata)
-           // 逻辑处理
-        } )
-	.catch( err=>console.log(err) )
+		let last = Math.floor(today / 1000)
+		fetch('/currency/kline?com_id=' + geturldata.coin + '_usdt&symbol=' + geturldata.coin + '&anchor=USDT&time=' + last + '&market_id=338&period=' + geturldata.day + '&timestamp=1674739035146&code=ebc161c4c01e448626c3cc30518009d6&platform=web_pc&v=1.0.0&language=en_US&legal_currency=USD')
+			.then(res => res.json())
+			.then(data => {
+				// console.log(data.data.kline)
+				const data_json = data.data.kline.reverse()
+				let klinedata = {
+					kline: data_json,
+					value: getklinevelue(data_json)
+				}
 
+				//console.log(JSON.stringify(klinedata))
+				setnewdata(klinedata.kline)
+				setnewdata2(klinedata.value)
+				setstoploding(true)
+				// console.log(newdata)
+				// 逻辑处理
+			})
+			.catch(err => console.log(err))
+	}
 
 	return (
 		<><div className='grid grid-cols-0 gap-3 place-content-center text-9xl'>{geturldata.coin}</div>
-		<div className='min-w-[1800px]  overflow-x-scroll'>
-			<div className='flew-shrink-0' id='newchart'>
-			<ChartComponent {...props} data={newdata} data2={newdata2}></ChartComponent>
-			</div>
+			<div className='min-w-[1800px]  overflow-x-auto '>
+				<div className='flew-shrink-0' id='newchart'>
+					<ChartComponent {...props} data={newdata} data2={newdata2}></ChartComponent>
+				</div>
 			</div></>
 	);
 }
