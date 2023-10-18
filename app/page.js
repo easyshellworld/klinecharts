52b855c404ecefe797be121ffffb218b41722abc
shell <shell@thisworld.club>
2023-10-18 20:58:54 +0800

    change Promise && add xlm

--- app/page.js
+++ app/page.js
@@ -7,47 +7,51 @@ import {   useState,useEffect } from 'react';
 
 export default function Home() {
  const [coindatas,setCoindats]=useState([]);
- let coindatas1={}
+ 
 
   const [loading,setloading]=useState(true)
-  
 
-  const coinnames=['btc', 'eth', 'matic665', 'apt530', 'tron', 'solana', 'UNI226','arb248','ape613']
-  
-  useEffect(()=>{
-    if(loading===true){
-      const num=coinnames.length
-      let count=0;
-      for (let i = 0; i < num; i++) {
-        coindatas1[coinnames[i]]={}
-       
-        getwebData({url:gettoday(coinnames[i])})
-          .then(data => {
-            //console.log(data.data)
-            const getdata = getlittledata(data.data.data.kline);
-            const atrdata = getATR(getdata);
-            coindatas1[coinnames[i]]= {
-              name: coinnames[i].match(/.*[a-zA-Z]/),
-              atr: atrdata,//getATR(getdata),
-              kdata: getdata
-            }
-            count++;
-           if(count>=num){
-            // console.log(coindatas1)
-            setCoindats(coindatas1)
-            setloading(false);
-           }
-        
-          }
-          ,err => {
-            return console.log(err);
-          })
-    
+const getcoinarr=async ()=>{
+  const coinnames=['BTC', 'ETH', 'matic665', 'apt530', 'tron', 'solana', 'UNI226','XLM','arb248','ape613']
+  const coinPromises=coinnames.map((coinname)=>{
+   return getwebData({url:gettoday(coinname)})
+    .then(data => {
+      const getdata = getlittledata(data.data.data.kline);
+      const atrdata = getATR(getdata);
+      const newname=coinname.match(/.*[a-zA-Z]/)
+      const coinprice= {
+        name: newname[0],
+        atr: atrdata,
+        kdata: getdata
       }
+       return coinprice
     }
+    ,err => {
+      return  console.log(err);
+    })
 
-    
- })
+  })
+  return Promise.all(coinPromises).then((coinPrices) => {
+    return coinPrices; 
+});
+
+}
+  
+
+  
+useEffect(()=>{
+  if(loading===true){
+  (async ()=>{
+
+      const coindata =await getcoinarr()    
+      setCoindats(coindata)
+      setloading(false) 
+  })()
+}
+  
+},[loading,coindatas])
+      
+  
 
     if (loading===true) return <div className='grid grid-cols-0 gap-8 place-content-center text-6xl h-full min-h-screen'>Loading...</div>
        
