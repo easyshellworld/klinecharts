56ab9e5936b969b9bd0f9eef09a9f81d25edff0f
shell <shell@thisworld.club>
2023-10-20 19:01:36 +0800

    fix coindatas and change changebutton

--- app/charts/commipt/changeurl.js
+++ app/charts/commipt/changeurl.js
@@ -11,7 +11,7 @@ export const Changeurl=()=>{
         <div className="flex justify-between">
             <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" value={coinname} onChange={e=>{setCoinname(e.target.value)}}/>
             <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" value={timename} onChange={e=>{setTimename(e.target.value)}}/>
-            <Link className="btn btn-succes" href={`/charts?coin=${coinname}&day=${timename}`}>change</Link>
+            <Link className="btn btn-succes bg-green-800" href={`/charts?coin=${coinname}&day=${timename}`}>change</Link>
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -49,7 +49,7 @@ useEffect(()=>{
   })()
 }
   
-},[loading,coindatas])
+},[loading])
       
   
 
